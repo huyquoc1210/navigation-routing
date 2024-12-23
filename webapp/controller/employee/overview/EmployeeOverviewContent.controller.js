e6967e3ecb62cf27b769578608297c9b45742634
huyquoc1210 <contact.huyquoc@gmail.com>
2024-12-23 11:59:54 +0700

    step14

--- webapp/controller/employee/overview/EmployeeOverviewContent.controller.js
+++ webapp/controller/employee/overview/EmployeeOverviewContent.controller.js
@@ -55,10 +55,17 @@ sap.ui.define(
             oQueryParameter.sortField,
             oQueryParameter.sortDescending
           );
+
+          // show dialog via URL hash
+          if (oQueryParameter.showDialog) {
+            this._oVSD.open();
+          }
         },
 
         onSortButtonPressed: function () {
-          this._oVSD.open();
+          var oRouter = this.getRouter();
+          this._oRouterArgs["?query"].showDialog = 1;
+          oRouter.navTo("employeeOverview", this._oRouterArgs);
         },
 
         onSearchEmployeesTable: function (oEvent) {
@@ -81,6 +88,15 @@ sap.ui.define(
               this._oRouterArgs["?query"].sortField = oSortItem.getKey();
               this._oRouterArgs["?query"].sortDescending =
                 oEvent.getParameter("sortDescending");
+              delete this._oRouterArgs["?query"].showDialog;
+              oRouter.navTo(
+                "employeeOverview",
+                this._oRouterArgs,
+                true /*without history*/
+              );
+            }.bind(this),
+            cancel: function (oEvent) {
+              delete this._oRouterArgs["?query"].showDialog;
               oRouter.navTo(
                 "employeeOverview",
                 this._oRouterArgs,
